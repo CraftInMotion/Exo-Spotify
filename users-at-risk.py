c15271ecaee222c9b667f2ad0c8569cfa4bf302c
CraftInMotion <iimiyux@hotmail.com>
2025-09-29 10:38:34 +0200

    fix(spotify)= changed some float to int

--- users-at-risk.py
+++ users-at-risk.py
@@ -8,8 +8,9 @@ user_at_risk = 0
 liste_user_at_risk = []
 
 for record in dataset:
-    if ((float(record ["skip_rate"]) > 0.3 and float(record ["listening_time"]) < 100)) or ((record ["subscription_type"] == "Free" and record ["offline_listening"] == "0" and float(record ["ads_listened_per_week"]) > 20)):
+    if ((float(record ["skip_rate"]) > 0.3 and int(record ["listening_time"]) < 100)) or ((record ["subscription_type"] == "Free" and int(record ["offline_listening"]) == 0 and int(record ["ads_listened_per_week"]) > 20)):
         user_at_risk = user_at_risk + 1
+        # à la place de faire une liste, j'aurais pu taper la commande print(record["user_id"])
         liste_user_at_risk.append(record ["user_id"])
 
 print(f"Vous devez relancer {user_at_risk} personnes.")
